56eebbb2263f22f39565c5ffb2de0b637ebadd2f
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-06-08 00:41:20 -0500

    Converted Js into one big function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 "use strict";
 
 var masterBracket=[
-  /*0*/["#1. Are my eyes blue?","#2. In my lifetime, did my family has only owned 1 pet?","#3. Am I currently working as a waiter when not in class?","#4. Is my favorite video game is The Last of Us by Naughty Dog?"
-    ,"#5. Have I never been out of the United States?"],
-  /*1*/["yes","no","yes", "yes", "no"],
-  /*2*/["y","n","y", "y", "n"],
-  /*3*/["correct response 1","correct response 2","correct response 3", "correct response 4", "correct response 5"],
-  /*4*/["inccorect response 1","inccorect response 2","inccorect response 3", "inccorect response 4", "inccorect response 5"]
+  /*Questions*/["#1. Are my eyes blue?","#2. In my lifetime, did my family has only owned 1 pet?","#3. Am I currently working as a waiter when not in class?",
+    "#4. Is my favorite video game is The Last of Us by Naughty Dog?","#5. Have I never been out of the United States?"],
+  /*correct Answer*/["yes","no","yes", "yes", "no"],
+  /*Alternitive Answer*/["y","n","y", "y", "n"],
+  /*Correct Response*/["Correct! Lucky guess/good observations! I can never remember other people's.","Yeah! Not counting the fish we have had 9 over the years","Survay says... Yes! I am a server at the coralville market grille.",
+    "Right! haha this question is too specific to be false","Correcto! I've been to Espana in 2015 and Canada in 2017"],
+  /*Incorrect Response*/["Sorry, they actually are. It's Okay, I can't see them either right now.","What?!? no way! I've had seven, not even counting all the fish.","On the weekends I work at one of market grilles at hyvee as a sever. Better luck on the next one.",
+    "Wrong. Yo have you even played this game. The story alone is so good!","Actually I've been lucky enough to be to Canada and Spain."]
 ];
 
 function quiz(){
@@ -14,13 +16,14 @@ function quiz(){
   var greet=confirm("Hello! would you like to take a quiz?");
   if(greet===false){
     alert("Too bad.");
+    var noFun=true;
   }
   var user=prompt("What's your name?");
-  while(user===null){
-    alert("you're still trying to cancel out of this?");
+  while(user===null&&noFun===true){
+    alert("you're trying to cancel out of this?");
     user=prompt("What's your name?");
   }
-  if (user===""){
+  if (user===""||user===null){
     alert("i'm just going to call you Bob");
     user=("Bob");
   }
@@ -28,193 +31,118 @@ function quiz(){
   var score=0;
   //Content Questions
   for(var questionNum=0;questionNum<5;questionNum++){
-    var eye=prompt(masterBracket[0][questionNum]).toLowerCase().trim();
-    if(eye===masterBracket[1][questionNum]||eye===masterBracket[2][questionNum]){
+    var userGuess=prompt(masterBracket[0][questionNum]).toLowerCase().trim();
+    console.log(masterBracket[0][questionNum]);
+    console.log("User guessed:"+userGuess);
+    if(userGuess===masterBracket[1][questionNum]||userGuess===masterBracket[2][questionNum]){
       alert(masterBracket[3][questionNum]);
       score++;
+      console.log("User was right.");
+    }
+    else if(userGuess===null){
+      break;
     }
     else{
       alert(masterBracket[4][questionNum]);
+      console.log("User was wrong.");
     }
+    console.log("User score:"+score);
   }
-}
-quiz();
-/*var score=7;
-
-var eye=prompt("#1. Are my eyes blue?").toLowerCase().trim();
-console.log("Are my eyes blue?");
-console.log("user answered:"+eye);
-
-if (eye==="y"||eye==="yes"){
-  alert("Correct! Lucky guess/good observations! I can never remember other people's.");
-  console.log("User was right.");
-}
-else if(eye==="n"||eye==="no"){
-  alert("Sorry, they actually are. It's Okay, I can't see them either right now.");
-  console.log("User was wrong.");
-  score=score-1;
-}
-else{
-  alert("i didn't register that.");
-  score=score-1;
-  console.log("User's imput was not understood.");
-}
-var pet=prompt("#2. In my lifetime, did my family has only owned 1 pet?").toLowerCase().trim();
-console.log("Do I have any pets?");
-console.log("user answered:"+pet);
-if (pet==="y"||pet==="yes"){
-  alert("What?!? no way! I've had seven, not even counting all the fish.");
-  score=score-1;
-  console.log("User was wrong.");
-}
-else if(pet==="n"||pet==="no"){
-  alert("Smart cookie. What kind of family only has one?");
-  console.log("User was right.");
-}
-else{
-  alert("i didn't register that.");
-  score=score-1;
-  console.log("User's imput was not understood.");
-}
-var job=prompt("#3. Am I currently working as a waiter when not in class?").toLowerCase().trim();
-console.log("Am I a waiter?");
-console.log("user answered:"+job);
-if (job==="y"||job==="yes"){
-  alert("Survay says... Yes! I am a server at the coralville market grille.");
-  console.log("User was right.");
-}
-else if(job==="n"||job==="no"){
-  alert("On the weekends I work at one of market grilles at hyvee as a sever. Better luck on the next one.");
-  score=score-1;
-  console.log("User was wrong.");
-}
-else{
-  alert("i didn't register that.");
-  score=score-1;
-  console.log("User's imput was not understood.");
-}
-var fav=prompt("#4. Is my favorite video game is The Last of Us by Naughty Dog?").toLowerCase().trim();
-console.log("Is my favorite game The Last of Us?");
-console.log("user answered:"+fav);
-if (fav==="y"||fav==="yes"){
-  alert("Right! haha this question is too specific to be false");
-  console.log("User was right.");
-}
-else if(fav==="n"||fav==="no"){
-  alert("Wrong. Yo have you even played this game. The story alone is so good!");
-  score=score-1;
-  console.log("User was wrong.");
-}
-else{
-  alert("i didn't register that.");
-  score=score-1;
-  console.log("User's imput was not understood.");
-}
-var out=prompt("#5. Have I never been out of the United States?").toLowerCase().trim();
-console.log("Have I never been out of the US?");
-console.log("user answered:"+out);
-if (out==="y"||out==="yes"){
-  alert("Actually I've been lucky enough to be to Canada and Spain.");
-  score=score-1;
-  console.log("User was wrong.");
-}
-if (out==="n"||out==="no"){
-  alert("Correcto! I've been to Espana in 2015 and Canada in 2017");
-  console.log("User was right.");
-}
-else{
-  alert("i didn't register that.");
-  score=score-1;
-  console.log("User's imput was not understood.");
-}
-*/
-var favNum=Math.floor(Math.random()*10).toString();
-console.log(favNum);
-var guess=prompt("Guess my favorite number between 0-9?");
-var lastGuess;
-var lastDistance;
-var currentDistance;
-var stat;
-var guessCount=6;
-while (guess!==favNum){
-  if (guess===""){
-    alert("I'm assuming that's a zero...");
+  if(userGuess===null){
+    return;
   }
-  if (guessCount===0||guess===null){
-    break;
-  }
-  if (!isNaN(lastGuess)){
-    lastDistance=lastGuess-favNum;
-    //console.log("current:"+lastDistance);
-    currentDistance=guess-favNum;
-    //console.log("this should not be running");
-    //console.log("current:"+currentDistance);
-    if (lastDistance<0){
-      lastDistance=lastDistance*(-1);
+  //Number Game <!--Still needs refactored -->
+  var favNum=Math.floor(Math.random()*10).toString();
+  console.log(favNum);
+  var guess=prompt("Guess my favorite number between 0-9?");
+  var lastGuess;
+  var lastDistance;
+  var currentDistance;
+  var stat;
+  var guessCount=6;
+  while (guess!==favNum){
+    if (guess===""){
+      alert("I'm assuming that's a zero...");
     }
-    if (currentDistance<0){
-      currentDistance=currentDistance*(-1);
+    if (guessCount===0||guess===null){
+      break;
     }
-    if (lastDistance>currentDistance){
-      stat="Warmer";
-    // console.log("last vs current:"+lastDistance+">"+currentDistance);
-    }
-    else if (lastDistance<currentDistance){
-      stat="Colder";
-    // console.log("last vs current:"+lastDistance+"<"+currentDistance);
-    }
-    else if(lastDistance===currentDistance){
-      stat="No change";
-      //console.log("last vs current:"+lastDistance+"="+currentDistance);
+    if (!isNaN(lastGuess)){
+      lastDistance=lastGuess-favNum;
+      //console.log("current:"+lastDistance);
+      currentDistance=guess-favNum;
       //console.log("this should not be running");
+      //console.log("current:"+currentDistance);
+      if (lastDistance<0){
+        lastDistance=lastDistance*(-1);
+      }
+      if (currentDistance<0){
+        currentDistance=currentDistance*(-1);
+      }
+      if (lastDistance>currentDistance){
+        stat="Warmer";
+      // console.log("last vs current:"+lastDistance+">"+currentDistance);
+      }
+      else if (lastDistance<currentDistance){
+        stat="Colder";
+      // console.log("last vs current:"+lastDistance+"<"+currentDistance);
+      }
+      else if(lastDistance===currentDistance){
+        stat="No change";
+        //console.log("last vs current:"+lastDistance+"="+currentDistance);
+        //console.log("this should not be running");
+      }
+      else{
+        stat=undefined;
+      }
+    }
+    lastGuess=guess;
+    if(stat===undefined){
+      guess=prompt("Try again? You still have "+guessCount+" guesses left");
     }
     else{
-      stat=undefined;
+      guess=prompt(stat+"! You wanna try again? You still have "+guessCount+" guesses left");
     }
+    console.log(guess);
+    guessCount=guessCount-1;
+    console.log(guessCount);
   }
-  lastGuess=guess;
-  if(stat===undefined){
-    guess=prompt("Try again? You still have "+guessCount+" guesses left");
+  if(guess===null){
+    alert("Give up already? The number was "+favNum);
+  }
+  else if(guessCount===0){
+    alert("Sorry, you ran out of guesses.");
   }
   else{
-    guess=prompt(stat+"! You wanna try again? You still have "+guessCount+" guesses left");
+    alert("You're right! The number was "+favNum+"!");
+    score++;
   }
-  console.log(guess);
-  guessCount=guessCount-1;
-  console.log(guessCount);
+  //Index guessing game
+  var bro=["kera","emily","matt","josh","chris"];
+  var guessBroCount=7;
+  var guessBro=prompt("Can you guess the name of one of my siblings?").toLowerCase();
+  console.log(bro);
+  console.log(guessBro);
+  console.log(guessBroCount);
+  console.log(bro.indexOf(guessBro));
+  while(bro.indexOf(guessBro)<0){
+    alert("Nope, sorry.");
+    guessBroCount-=1;
+    if(guessBroCount===0){
+      break;
+    }
+    guessBro=prompt("Wanna try agian? You have "+guessBroCount+" tries left.");
+  }
+  if (guessBroCount===0){
+    alert("Oh no! You ran out of guesses. My siblings' names are: "+bro.join(", "));
+  }
+  else{
+    alert("Good guess! I have 5 siblings, "+bro.join(", "));
+    score++;
+  }
+  alert("Thanks for playing! "+user+" You got "+score+"/7 questions right!");
 }
-if(guess===null){
-  alert("Give up already? The number was "+favNum);
-  score=score-1;
-}
-else if(guessCount===0){
-  alert("Sorry, you ran out of guesses.");
-  score=score-1;
-}
-else{
-  alert("You're right! The number was "+favNum+"!");
-}
+quiz();
 
-var bro=["kera","emily","matt","josh","chris"];
-var guessBroCount=7;
-var guessBro=prompt("Can you guess the name of one of my siblings?").toLowerCase();
-console.log(bro);
-console.log(guessBro);
-console.log(guessBroCount);
-console.log(bro.indexOf(guessBro));
-while(bro.indexOf(guessBro)<=0){
-  alert("Nope, sorry.");
-  guessBroCount-=1;
-  if(guessBroCount===0){
-    break;
-  }
-  guessBro=prompt("Wanna try agian? You have "+guessBroCount+" tries left.");
-}
-if (guessBroCount===0){
-  alert("Oh no! You ran out of guesses. My siblings' names are: "+bro.join(", "));
-  score=score-1;
-}
-else{
-  alert("Good guess! I have 5 siblings, "+bro.join(", "));
-}
-alert("Thanks for playing! "+user+" You got "+score+"/7 questions right!");
+
+
